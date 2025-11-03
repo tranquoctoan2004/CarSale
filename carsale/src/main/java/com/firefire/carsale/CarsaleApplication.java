396adfee43e1a47c8ea72d675898e66ec9dbba74
ToanTran <toan1112004@gmail.com>
2025-11-03 15:47:53 +0700

    setting envi + demo

---
+++ carsale/src/main/java/com/firefire/carsale/CarsaleApplication.java
@@ -0,0 +1,13 @@
+package com.firefire.carsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsaleApplication.class, args);
+	}
+
+}
---
+++ carsale/src/test/java/com/firefire/carsale/CarsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firefire.carsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
