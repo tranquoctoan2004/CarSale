7c0a954f54864c6202791fd9fb17da71f5133dab
ToanTran <toan1112004@gmail.com>
2026-01-01 03:22:04 +0700

    exception

---
+++ carsale/src/main/java/com/firefire/carsale/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.firefire.carsale.exception;
+
+public class ConflictException extends BusinessException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
