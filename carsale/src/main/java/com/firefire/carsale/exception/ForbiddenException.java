7eeaa0fc82badff054afd8325c7b37c77c3481d0
ToanTran <toan1112004@gmail.com>
2026-01-01 03:22:24 +0700

    exception error

---
+++ carsale/src/main/java/com/firefire/carsale/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package com.firefire.carsale.exception;
+
+public class ForbiddenException extends BusinessException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
