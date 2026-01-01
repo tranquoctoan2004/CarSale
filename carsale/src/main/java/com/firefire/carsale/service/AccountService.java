a8d833c5be8c6bd2720258fbf0489a361e1218d3
ToanTran <toan1112004@gmail.com>
2026-01-02 01:44:56 +0700

    account controller

--- carsale/src/main/java/com/firefire/carsale/service/AccountService.java
+++ carsale/src/main/java/com/firefire/carsale/service/AccountService.java
@@ -2,6 +2,8 @@ package com.firefire.carsale.service;
 
 import java.util.List;
 
+import com.firefire.carsale.dto.filter.UserSearchFilter;
+import com.firefire.carsale.dto.request.AdminCreateUserRequest;
 import com.firefire.carsale.dto.request.AdminUpdateUserRequest;
 import com.firefire.carsale.dto.response.AccountResponse;
 
@@ -13,5 +15,9 @@ public interface AccountService {
     void deleteAccount(Integer id);
 
     void adminUpdateUser(Integer id, AdminUpdateUserRequest request);
+
+    List<AccountResponse> searchAccounts(UserSearchFilter filter, int page, int size);
+
+    void adminCreateUser(AdminCreateUserRequest request);
 
 }
